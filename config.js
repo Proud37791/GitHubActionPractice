ae9da0b5f47e669057b46ba7a489a49de05ce99f
np.proud <proud37791@gmail.com>
2025-04-24 13:50:31 +0700

    config

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
-const crypto = require("crypto");
-const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development', // or 'production'
+};
